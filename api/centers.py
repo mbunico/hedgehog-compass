ca560062f5901548cf3a09f89eefe7f926a688e8
mark <116787082+mbunico@users.noreply.github.com>
2026-03-09 23:52:54 +0800

    Update centers.py

--- api/centers.py
+++ api/centers.py
@@ -11,7 +11,7 @@ app = FastAPI(title="Autism Centers – OSM Backend")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://hedgehog-compass.vercel.app",
+        "http://hedgehog-compass.vercel-v1.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
